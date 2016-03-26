c7562ddbfd46a1751a16b16813bbdf07e5c89044
CruzanCaramele <letters2hamza@gmail.com>
2016-03-26 09:30:49 +0800

    add users

--- aws/iam.tf
+++ aws/iam.tf
@@ -6,3 +6,16 @@ provider "aws" {
 	secret_key = "${var.aws_secret_key}"
 	region     = "us-east-1" 
 }
+
+#--------------------------------------------------------------
+# User(s)
+#--------------------------------------------------------------
+resource "aws_iam_user" "admin-sudo" {
+	name = "admin-sudo"
+	path = "/"
+}
+
+resource "aws_iam_user" "developer" {
+	name = "developer"
+	path = "/"
+}
