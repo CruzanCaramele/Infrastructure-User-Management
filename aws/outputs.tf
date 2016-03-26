77235651a492249f0a40c6d8fe99c16c0a9ba097
CruzanCaramele <letters2hamza@gmail.com>
2016-03-26 11:20:01 +0800

    seperate outputs for developer and admin

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,45 +1,45 @@
 #--------------------------------------------------------------
 # Admin User outputs
 #--------------------------------------------------------------
-output "user_id" {
+output "admin_id" {
   value = "${aws_iam_user.adminSudo.id}"
 }
 
-output "user_arn" {
+output "admin_arn" {
   value = "${aws_iam_user.adminSudo.arn}"
 }
 
-output "user_name" {
+output "admin_name" {
   value = "${aws_iam_user.adminSudo.name}"
 }
 
-output "user_access_key_id" {
+output "admin_access_key_id" {
   value = "${aws_iam_access_key.sudoKey.id}"
 }
 
-output "user_access_key_secret" {
+output "admin_access_key_secret" {
   value = "${aws_iam_access_key.sudoKey.secret}"
 }
 
 #--------------------------------------------------------------
 # Developer User outputs
 #--------------------------------------------------------------
-output "user_id" {
+output "developer_id" {
   value = "${aws_iam_user.developer.id}"
 }
 
-output "user_arn" {
+output "developer_arn" {
   value = "${aws_iam_user.developer.arn}"
 }
 
-output "user_name" {
+output "developer_name" {
   value = "${aws_iam_user.developer.name}"
 }
 
-output "user_access_key_id" {
+output "developer_access_key_id" {
   value = "${aws_iam_access_key.developerKey.id}"
 }
 
-output "user_access_key_secret" {
+output "developer_access_key_secret" {
   value = "${aws_iam_access_key.developerKey.secret}"
 }
